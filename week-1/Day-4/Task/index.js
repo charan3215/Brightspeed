bffd97e7adb661c369aa6ed56bdcac7a5df11ce1
charan3215 <131665714+charan3215@users.noreply.github.com>
2023-12-15 10:50:16 +0530

    Add files via upload

---
+++ week-1/Day-4/Task/index.js
@@ -0,0 +1,54 @@
+// let s=document.getElementById('st');
+
+// s.style.color='blue';
+
+
+// // s.innerHTML="<p style='color:blue;'>Bye</p>";
+// console.log(s);
+// console.log(s.textContent);
+// console.log(s.textContent);
+
+// let g=document.getElementsByClassName('dt');
+
+// let ts= document.getElementsByTagName('h3')
+// // console.log(ts);
+// ts[0].style.color='green';
+// ts[1].style.color='yellow';
+
+// let gg=document.getElementById('but');
+
+// let int=document.getElementById('in');
+
+// function storeTxt(event)
+// {
+//     event.preventDefault();
+//     console.log(int.value);
+// }
+
+
+// gg.addEventListener('click',storeTxt);
+
+
+//2nd --->
+let sp=document.getElementById('he');
+
+// function Eve()
+// {
+//     sp.style.color='green';
+// }
+
+
+// sp.addEventListener('click',Eve);
+
+// 3-->
+
+function add()
+{
+    sp.innerHTML='charan';
+
+}
+
+sp.addEventListener('click',add)
+
+
+//4-->
